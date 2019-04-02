11e6ae67100f4fcf62f8988892d15b461b664885
Thomas Hasler <froorider@gmail.com>
2019-04-02 20:05:51 +0200

    added swagger. added gitignore

--- src/main/java/com/aws/codestar/projecttemplates/Application.java
+++ src/main/java/com/aws/codestar/projecttemplates/Application.java
@@ -2,6 +2,13 @@ package com.aws.codestar.projecttemplates;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /** Simple class to start up the application.
  *
@@ -11,9 +18,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *  @ComponentScan
  */
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
     }
 }
